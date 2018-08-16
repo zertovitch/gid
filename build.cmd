677c092f1fde56588bf0cefa67ee6e137b71d924
gdemont <gdemont@43dcbad6-2c11-4310-8ab3-e0ca0165eabd>
2018-08-16 15:52:47 +0000

    Simplified GNAT build in Debug mode (new built-in trace-back)

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
-gprbuild -p -P gid -XBuild_Mode=Debug tb
+gprbuild -p -P gid -XBuild_Mode=Debug to_bmp
+copy test\to_bmp.exe test\tb.exe
 gprbuild -p -P gid -XBuild_Mode=Fast
 gprbuild -p -P gid -XBuild_Mode=Smallest mini
--- test/tb.ads
+++
@@ -1,6 +0,0 @@
--- Build with -bargs -E
-
-with TB_Wrap, To_BMP;
-pragma Elaborate_All(TB_Wrap);
-
-procedure TB is new TB_Wrap(To_BMP);
