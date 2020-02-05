a2e3bb54119c617400a544e34f06440f7f583733
unknown <gdemont@hotmail.com>
2020-02-05 09:07:53 +0100

    (doc/scripts) adapted to 'GID_Build_Mode'

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-gprbuild -p -P gid -XBuild_Mode=Debug to_bmp
+gprbuild -p -P gid -XGID_Build_Mode=Debug to_bmp
 copy test\to_bmp.exe test\tb.exe
-gprbuild -p -P gid -XBuild_Mode=Fast
-gprbuild -p -P gid -XBuild_Mode=Smallest mini
+gprbuild -p -P gid -XGID_Build_Mode=Fast
+gprbuild -p -P gid -XGID_Build_Mode=Smallest mini
