8f3e58b70076360be1503804e620560b026841dd
zertovitch <gdemont@hotmail.com>
2023-08-21 22:35:43 +0200

    (tests/tools) Improved Steg's messages about possible rescaling of image

--- test/steg.adb
+++ test/steg.adb
@@ -5,21 +5,24 @@
 --
 
 --  To do:
---  - encryption
+--    - encryption
 
 with GID;
 
-with Ada.Calendar;
-with Ada.Characters.Handling;           use Ada.Characters.Handling;
-with Ada.Command_Line;                  use Ada.Command_Line;
-with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
-with Ada.Streams.Stream_IO;             use Ada.Streams.Stream_IO;
-with Ada.Text_IO;                       use Ada.Text_IO;
-with Ada.Unchecked_Deallocation;
+with Ada.Calendar,
+     Ada.Characters.Handling,
+     Ada.Command_Line,
+     Ada.Float_Text_IO,
+     Ada.Numerics.Elementary_Functions,
+     Ada.Streams.Stream_IO,
+     Ada.Text_IO,
+     Ada.Unchecked_Deallocation;
 
 with Interfaces;
 
 procedure Steg is
+
+  use Ada.Characters.Handling, Ada.Streams.Stream_IO, Ada.Text_IO;
 
   procedure Blurb is
   begin
@@ -126,71 +129,88 @@ procedure Steg is
     --
     procedure Encode is
       idx : Natural := img_buf'Last;
-      --  Start with buffer's end (image's bottom), with the hope it is "noisier":
-      --  often there is a blue sky or something like that on the top...
-      procedure Encode_byte (b : Unsigned_8) is
+      --
+      --  Start with buffer's end (the image's bottom), with the hope it is "noisier":
+      --  often there is a blue sky, or some smooth background like that, on the image's top...
+      --
+      procedure Encode_Byte (b : Unsigned_8) is
       begin
-        img_buf (idx) := (img_buf (idx) and 2#1111_1100#) or (b and 2#0000_0011#);                --  B
+        --  Blue:
+        img_buf (idx) := (img_buf (idx) and 2#1111_1100#) or (b and 2#0000_0011#);
         idx := idx - 1;
-        img_buf (idx) := (img_buf (idx) and 2#1111_1000#) or Shift_Right (b and 2#0001_1100#, 2);  --  G
+        --  Green:
+        img_buf (idx) := (img_buf (idx) and 2#1111_1000#) or Shift_Right (b and 2#0001_1100#, 2);
         idx := idx - 1;
-        img_buf (idx) := (img_buf (idx) and 2#1111_1000#) or Shift_Right (b, 5);                   --  R
+        --  Red:
+        img_buf (idx) := (img_buf (idx) and 2#1111_1000#) or Shift_Right (b, 5);
         idx := idx - 1;
-      end Encode_byte;
+      end Encode_Byte;
+      --
       b : Unsigned_8;
       data_size : Unsigned_64;
       needed_size : Unsigned_64;
       available_size : constant Unsigned_64 := img_buf'Length / 3;  --  1 byte per pixel;
       factor : Float;
+      use Ada.Float_Text_IO, Ada.Numerics.Elementary_Functions;
+      function Percents_Image (x : Float) return String is (Integer (100.0 * x)'Image & '%');
+      function Suggested_Scaling_Percents return String is (Percents_Image (Sqrt (factor)));
     begin
       Open (f_dt, In_File, data_name);
       data_size := Unsigned_64 (Size (f_dt));
       needed_size := data_size + 8;
       factor := Float (needed_size) / Float (available_size);
       if needed_size > available_size then
+        factor := factor + 0.01;  --  Adjust for avoiding exception message with exactly "100%".
         raise Data_too_large with
-          "Needs a" & Integer'Image (1 + Integer (100.0 * factor)) &
-          "% raw size scaling, i.e. a" &
-          Integer'Image (1 + Integer (100.0 * Sqrt (factor))) &
-          "% image scaling in both dimensions";
+          "Needs around" & Percents_Image (factor) & " raw size scaling, i.e. around" &
+          Suggested_Scaling_Percents & " image scaling in both dimensions";
       end if;
-      Put_Line (Standard_Error,
-        "Data size:" & Unsigned_64'Image (data_size) &
-        ", using" & Integer'Image (Integer (100.0 * factor)) &
-        "% of image data"
-      );
+      Put (Standard_Error, "Data size:" & data_size'Image & ", using ");
+      Put (Standard_Error, 100.0 * factor, 0, 3, 0);
+      Put_Line (Standard_Error,  "% of image data");
+      if factor < 0.98 then
+        Put_Line
+          (Standard_Error,
+           "You could still encode the data on a reduced image, scaled down to" &
+           Suggested_Scaling_Percents & " in both dimensions");
+      end if;
       for i in 1 .. 8 loop
-        Encode_byte (Unsigned_8 (data_size and 16#FF#));
+        Encode_Byte (Unsigned_8 (data_size and 16#FF#));
         data_size := Shift_Right (data_size, 8);
       end loop;
       while not End_Of_File (f_dt) loop
         Unsigned_8'Read (Stream (f_dt), b);
-        Encode_byte (b);
+        Encode_Byte (b);
       end loop;
       Close (f_dt);
     end Encode;
     --
     procedure Decode is
       idx : Natural := img_buf'Last;
-      procedure Decode_byte (b : out Unsigned_8) is
+      --
+      procedure Decode_Byte (b : out Unsigned_8) is
       begin
-        b := img_buf (idx) and 2#0000_0011#;                     --  B
+        --  Blue:
+        b := img_buf (idx) and 2#0000_0011#;
         idx := idx - 1;
-        b := b + Shift_Left (img_buf (idx) and 2#0000_0111#, 2);  --  G
+        --  Green:
+        b := b + Shift_Left (img_buf (idx) and 2#0000_0111#, 2);
         idx := idx - 1;
-        b := b + Shift_Left (img_buf (idx) and 2#0000_0111#, 5);  --  R
+        --  Red:
+        b := b + Shift_Left (img_buf (idx) and 2#0000_0111#, 5);
         idx := idx - 1;
-      end Decode_byte;
+      end Decode_Byte;
+      --
       b : Unsigned_8;
       data_size : Unsigned_64 := 0;
     begin
       for i in 0 .. 7 loop
-        Decode_byte (b);
+        Decode_Byte (b);
         data_size := data_size + Shift_Left (Unsigned_64 (b), i * 8);
       end loop;
       Create (f_dt, Out_File, data_name);
       for i in 1 .. data_size loop
-        Decode_byte (b);
+        Decode_Byte (b);
         Unsigned_8'Write (Stream (f_dt), b);
       end loop;
       Close (f_dt);
@@ -229,6 +249,8 @@ procedure Steg is
   end Process;
 
   op : Operation;
+
+  use Ada.Command_Line;
 
 begin
   if Argument_Count /= 3 then
