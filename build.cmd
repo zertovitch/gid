cbfaa1e8381dd7fc73bb88bffe04e73cde0f4b50
unknown <gdemont@hotmail.com>
2024-03-05 02:46:06 +0100

    Cleanup in scripts

--- build.cmd
+++ build.cmd
@@ -3,24 +3,24 @@ echo ** Building GID in various compiler modes
 echo.
 echo **** To_BMP - Debug ... tb.exe
 del test\to_bmp.exe
-gprbuild -p -P gid -XGID_Build_Mode=Debug to_bmp
+gprbuild -P gid -XGID_Build_Mode=Debug to_bmp
 copy /b test\to_bmp.exe test\tb.exe
 
 echo.
-echo **** To_BMP - Fast_unchecked ... tbfu.exe
+echo **** To_BMP - Fast_unchecked ... tbfu.exe; all other programs.
 del test\to_bmp.exe
-gprbuild -p -P gid -XGID_Build_Mode=Fast_unchecked
+gprbuild -P gid -XGID_Build_Mode=Fast_unchecked
 copy /b test\to_bmp.exe test\tbfu.exe
 
 echo.
 echo **** To_BMP - Fast_but_checked ... tbfc.exe
 del test\to_bmp.exe
-gprbuild -p -P gid -XGID_Build_Mode=Fast_but_checked to_bmp
+gprbuild -P gid -XGID_Build_Mode=Fast_but_checked to_bmp
 copy /b test\to_bmp.exe test\tbfc.exe
 
 echo.
 echo **** Smallest ... mini.exe
 del test\mini.exe
-gprbuild -p -P gid -XGID_Build_Mode=Smallest mini
+gprbuild -P gid -XGID_Build_Mode=Smallest mini
 
 pause
