0d326666ddaf05b442f34fdc0057594b6caf3c1e
unknown <gdemont@hotmail.com>
2024-08-17 13:49:02 +0200

    Gardening

--- test/fast_io.ads
+++ test/fast_io.ads
@@ -7,6 +7,9 @@ generic
 
 package Fast_IO is
 
+  --  `Read` needs some extra info not available with Ada.Streams.Root_Stream_Type.
+  --  See zip.adb of the Zip-Ada project for an implementation. 
+
   procedure Write (s : in out Ada.Streams.Root_Stream_Type'Class; b : in Byte_Array);
 
 end Fast_IO;
